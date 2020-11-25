729f265c6a20e3323d9b22e633c10afffea16e8f
Владелец <leonid82@yandex.ru>
2020-11-25 15:27:41 +0300

    2

--- Problems/Convert a date/src/Main.java
+++ Problems/Convert a date/src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Arrays;
+import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         // put your code here
+        Scanner scanner = new Scanner(System.in);
+        String data = scanner.nextLine();
+        String[] arr = data.split("-");
+        System.out.println(arr[1] + "/" + arr[2] + "/" + arr[0]);
     }
 }
